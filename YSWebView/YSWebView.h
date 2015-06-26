454703acbe2baaa3ff4e84215985f23ecf9231f4
Bogdan Poplauschi <bpoplauschi@gmail.com>
2015-06-26 15:14:31 +0300

    Initial Commit

---
+++ YSWebView/YSWebView.h
@@ -0,0 +1,13 @@
+//
+//  YSWebView.h
+//  YSWebView
+//
+//  Created by Bogdan Poplauschi on 26/06/15.
+//  Copyright (c) 2015 Yardi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YSWebView : NSObject
+
+@end
---
+++ YSWebView/YSWebView.m
@@ -0,0 +1,13 @@
+//
+//  YSWebView.m
+//  YSWebView
+//
+//  Created by Bogdan Poplauschi on 26/06/15.
+//  Copyright (c) 2015 Yardi. All rights reserved.
+//
+
+#import "YSWebView.h"
+
+@implementation YSWebView
+
+@end
